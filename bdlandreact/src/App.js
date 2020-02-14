069c814d3d2af63ad2ac484a4f8921264d7ed6b9
gauthier <gauthiermolard421@hotmail.com>
2020-02-14 16:19:59 +0100

    Modif Formulaire

--- bdlandreact/src/App.js
+++ bdlandreact/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Head} from "./components/Head"
 import './App.css';
+import {Formulaire} from  "./components/Formulaire"
 
 function App() {
   return (
     <div>
       <Head/>
+      <Formulaire></Formulaire>
     </div>
   );
 }
--- bdlandreact/src/components/Formulaire.js
+++ bdlandreact/src/components/Formulaire.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Nav, Navbar, Form, FormControl, Button, Dropdown, DropdownButton } from 'react-bootstrap'
+
 
 
 
 export const Formulaire = () => {
-    
+
     return (
         <div>
 
-            <form>
+
+            <Form className="Formulaire" >
 
                 <div class="row">
-                    <label class="label">Titre :</label>
-                    <input class="controle" type="text" name="Titre" />
+                    <input class="form-control" type="text" name="Titre" placeholder = "Titre" />
                     <span class="resultat"></span>
                 </div>
 
@@ -21,80 +23,73 @@ export const Formulaire = () => {
 
 
                 <div class="row">
-                    <label for="Auteur"> Auteur :</label>
-                    <input class="controle" type="text" name="Auteur" />
+                   
+                    <input class="form-control" type="text" name="Auteur"  placeholder = "Auteur" />
                     <span class="resultat"></span>
 
                 </div>
 
 
                 <div class="row">
-                    <label for="Description"> Description :</label>
-                    <input class="controle" type="text" name="Description" />
+                {/* <input class="controle" type="text" name="Description"  placeholder = "Description" /> */}
+                    <textarea class="form-control" rows="2" id="comment" placeholder = "Description" ></textarea>
                     <span class="resultat"></span>
 
                 </div>
 
                 <div class="row">
-                    <label for="Categorie"> Categorie :</label>
+                  {/* <label for = "Categorie">Categorie</label> */}
+                    
 
-                    <select name="Categorie" >
-                        <option>Action</option>
-                        <option>Humour</option>
-                        <option>Historique</option>
-                        <option>Manga</option>
-                        <option>Science fiction</option>
-                    </select>
+
+                    <DropdownButton className="mr-sm-2" variant="danger"
+                        id="dropdown-basic-button" title="Catégorie">
+                        <Dropdown.Item href="#/action-1">Western</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Aventure</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Humour</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1">Historique</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">SF</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Manga</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Fantastique</Dropdown.Item>
+                    </DropdownButton>
+
                 </div>
-
 
                 <div class="row">
-                    <label for="FormulaireModif"> Formulaire Modif :</label>
-                    <select name="FormulaireModif" >
-                        <option>Ajouter une oeuvre</option>
-                        <option>Supprimer une oeuvre</option>
-                        <option>Modifier oeuvre</option>
-                        <a class="btn btn-primary" href="#" role="button">Link</a>
-                        
-                    </select>
+
+                   
+                 
+                    
+
+                    <CKEditor
+
+
+                        editor={ClassicEditor}
+
+                        onInit={editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log('Editor is ready to use!', editor);
+                        }}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            console.log({ event, editor, data });
+                        }}
+                        onBlur={(event, editor) => {
+                            console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            console.log('Focus.', editor);
+                        }}
+                    />
+
+
                 </div>
-
-
-
-            
-                
-                <label for="Contenu"> Contenu  :</label>
-
-                <CKEditor
-
-
-
-
-                    editor={ClassicEditor}
-
-                    onInit={editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log('Editor is ready to use!', editor);
-                    }}
-                    onChange={(event, editor) => {
-                        const data = editor.getData();
-                        console.log({ event, editor, data });
-                    }}
-                    onBlur={(event, editor) => {
-                        console.log('Blur.', editor);
-                    }}
-                    onFocus={(event, editor) => {
-                        console.log('Focus.', editor);
-                    }}
-                />
-
-            </form>
-
-
+                <Button variant="success">Creer</Button>
+            </Form>
 
 
         </div>
-        )
-    
+    )
+
 
 }

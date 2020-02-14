aa3585c3954c05dd5b2691fa4de5c7db2a926af4
med <mrsouali@gmail.com>
2020-02-14 11:40:38 +0100

    Ajout du fichier sql pour la creation de la BDD

---
+++ SqlPourBDD.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE categorie (
+id INT PRIMARY KEY IDENTITY(1,1),
+categorie_type VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE bd (
+id INT PRIMARY KEY IDENTITY(1,1),
+titre VARCHAR(50) NOT NULL,
+bd_description VARCHAR(800) NOT NULL,
+autheur VARCHAR(50) NOT NULL,
+date_post DATETIME NOT NULL,
+contenu NVARCHAR(max) NOT NULL,
+id_categorie INT NOT NULL,
+FOREIGN KEY (id_categorie) REFERENCES categorie (id)
+)
+
+INSERT INTO categorie (categorie_type) VALUES ('action'), ('historique'), ('manga'), ('science-fiction'), ('hantai');

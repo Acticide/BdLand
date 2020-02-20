8a1470501ddf09c2ee83e4b09be83afdd0e0ff68
gauthier <gauthiermolard421@hotmail.com>
2020-02-20 09:48:49 +0100

    lien cliquable

--- bdlandreact/src/components/Footer.js
+++ bdlandreact/src/components/Footer.js
@@ -15,17 +15,26 @@ export const Footer = () => {
             <div class="reseaux">
                 <h1> Suivez nous sur : </h1>
                 <ol>
-                    <i class="fab fa-facebook-f"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fab fa-snapchat"></i>
-                    <i class="fab fa-pinterest-square"></i>
-                    <i class="fab fa-linkedin"></i>
-                    <i class="fab fa-viadeo"></i>
-                    <i class="fab fa-slack"></i>
-                    <i class="fab fa-flickr"></i>
-                    <i class="fab fa-youtube"></i>
-                    <i class="fab fa-twitch"></i>
+                   
+                    
+                   
+                   
+                  
+                   
+                    
+                   
+                
+                  
+                    <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/?hl=fr"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.snapchat.com/l/fr-fr/"><i class="fab fa-snapchat"></i></a>
+                    <a href="https://www.pinterest.fr/"><i class="fab fa-pinterest-square"></i> </a>
+                    <a href="https://fr.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://slack.com/intl/fr-fr/"><i class="fab fa-slack"></i></a>
+                    <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                    <a href="https://www.twitch.tv/"><i class="fab fa-twitch"></i> </a>
+                    <a href="https://twitter.com/login?lang=fr"><i class="fab fa-twitter"></i></a>
+                    
 
                 </ol>
 
